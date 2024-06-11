327112dda2b620d524f672934dd09d0ae0a50795
jesuemmanuel <jesutlaltecatl@gmail.com>
2024-06-11 16:56:54 -0600

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "id22199869_saemda";
+$password = "caNijo17@/";
+$dbname = "id22199869_todo_list";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+/*if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}*/
+?>
